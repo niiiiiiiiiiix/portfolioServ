cf633b928a6f19e90db609b0baf9d44431c3cdfa
Nix Tay <niiiiiiiiiiix@users.noreply.github.com>
2023-03-07 02:55:28 +0800

    Refactor homeground GET

--- src/routes/Beans.route.ts
+++ src/routes/Beans.route.ts
@@ -3,9 +3,10 @@ import axios from 'axios';
 import * as cheerio from 'cheerio';
 
 const beans = express.Router();
-const HOMEGROUND_URL =
-  'https://homegroundcoffeeroasters.com/collections/filter-coffee';
-const ALCHEMIST_URL = 'https://alchemist.com.sg/shop-coffee-beans/filter';
+const HOMEGROUND_BASE_URL = 'https://homegroundcoffeeroasters.com';
+const HOMEGROUND_FILTER_PATH = '/collections/filter-coffee';
+const ALCHEMIST_BASE_URL = 'https://alchemist.com.sg';
+const ALCHEMIST_FILTER_PATH = '/shop-coffee-beans/filter';
 
 beans.get('/', (request, response, next) => {
   response.send('Welcome');
@@ -13,12 +14,14 @@ beans.get('/', (request, response, next) => {
 
 beans.get('/homeground', async (request, response, next) => {
   try {
-    const listOfBeans = await axios.get(HOMEGROUND_URL);
+    const listOfBeans: axios.AxiosResponse = await axios.get(
+      HOMEGROUND_BASE_URL + HOMEGROUND_FILTER_PATH
+    );
     const $ = cheerio.load(listOfBeans.data);
-    const listItems = $('.grid-product__link')
+    const beansPathArray = $('.grid-product__link')
       .get()
       .map((bean) => $(bean).attr('href'));
-    const fullDetails = await fetchFullDetails(listItems);
+    const fullDetails = await fetchFullDetails(beansPathArray);
     response.status(200).json(fullDetails);
   } catch (error) {
     error.statusCode = 400;
@@ -26,45 +29,24 @@ beans.get('/homeground', async (request, response, next) => {
   }
 });
 
-const fetchFullDetails = async (listItems) => {
-  const finalArray = [];
-  for (let i = 0; i < listItems.length; i++) {
-    const itemUrl = 'https://homegroundcoffeeroasters.com' + listItems[i];
-    const allDetails = await axios.get(itemUrl);
-    const $ = cheerio.load(allDetails.data);
-    const itemName: string = $('.product-single__title').text().trim();
-    const getData = $('.shogun-heading-component > h1')
-      .get()
-      .map((x) => $(x).text().replace(/[\n]/g, '').trim());
-    const tastingNotes: string = getData[1];
-    const coffee: string = getData[3];
-    const region: string = getData[5];
-    const origin: string = getData[7];
-    const process: string = getData[9];
-    const varietal: string = getData[11];
-    const weightPriceMap: { [itemWeight: string]: string } = {};
-    const getSomeData = JSON.parse($('textarea').text().trim());
-    for (const data of getSomeData) {
-      let itemWeight = data.title;
-      let itemPrice = convertValue(data.price / 100);
-      weightPriceMap[itemWeight] = itemPrice;
-    }
-    finalArray.push({
-      itemName: itemName,
-      itemUrl: itemUrl,
-      tastingNotes: tastingNotes,
-      coffee: coffee,
-      region: region,
-      origin: origin,
-      process: process,
-      varietal: varietal,
-      weightPriceMap,
+const fetchFullDetails = async (beansPathArray) => {
+  const beansInformation = [];
+  for (let i = 0; i < beansPathArray.length; i++) {
+    const link: string = HOMEGROUND_BASE_URL + beansPathArray[i];
+    const content: axios.AxiosResponse = await axios.get(link);
+    const $: cheerio.CheerioAPI = cheerio.load(content.data);
+    const name: string = $('.product-single__title').text().trim();
+    const details = createDetailsObject($);
+    beansInformation.push({
+      name: name,
+      link: link,
+      details,
     });
   }
-  return finalArray;
+  return beansInformation;
 };
 
-const convertValue = (value) => {
+const formatPriceData = (value) => {
   return new Intl.NumberFormat('en-SG', {
     style: 'currency',
     currency: 'SGD',
@@ -72,9 +54,53 @@ const convertValue = (value) => {
   }).format(value);
 };
 
+const createDetailsObject = ($) => {
+  const details: {} = $('.shogun-heading-component > h1')
+    .get()
+    .map((detail) => $(detail).text().replace(/[\n]/g, '').trim())
+    .reduce(
+      (
+        detailsObject: {},
+        currentValue: string,
+        currentIndex: number,
+        array: string[]
+      ) => {
+        if (currentIndex & 1) {
+          detailsObject[convertToCamelCase(array[currentIndex - 1])] =
+            currentValue;
+        }
+        return detailsObject;
+      },
+      {}
+    );
+  const pricing: { [weight: string]: string } = {};
+  const getTextAreaData = JSON.parse($('textarea').text().trim());
+  for (const data of getTextAreaData) {
+    let weight = data.title;
+    let price = formatPriceData(data.price / 100);
+    pricing[weight] = price;
+  }
+  details['pricing'] = pricing;
+  return details;
+};
+
+const convertToCamelCase = (string) => {
+  const returnArray: string[] = [];
+  string.split(' ').forEach((word, index) => {
+    if (index === 0) {
+      returnArray.push(word.toLowerCase());
+    } else {
+      returnArray.push(word);
+    }
+  });
+  return returnArray.join('');
+};
+
 beans.get('/alchemist', async (request, response, next) => {
   try {
-    const { data } = await axios.get(ALCHEMIST_URL);
+    const { data } = await axios.get(
+      ALCHEMIST_BASE_URL + ALCHEMIST_FILTER_PATH
+    );
     const $ = cheerio.load(data);
     const listItems = $('.hentry');
     let results: string[][] = []!;
