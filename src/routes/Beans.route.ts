cfa881df0cc9bc05f81ae1bb6ecf8db190f3e37c
Nix Tay <niiiiiiiiiiix@users.noreply.github.com>
2023-03-28 04:05:09 +0800

    Implement crude version of details object for Alchemist

--- src/routes/Beans.route.ts
+++ src/routes/Beans.route.ts
@@ -21,7 +21,10 @@ beans.get('/homeground', async (request, response, next) => {
     const beansPathArray = $('.grid-product__link')
       .get()
       .map((bean) => $(bean).attr('href'));
-    const fullDetails = await fetchFullDetails(beansPathArray);
+    const fullDetails = await fetchFullDetails(
+      beansPathArray,
+      HOMEGROUND_BASE_URL
+    );
     response.status(200).json(fullDetails);
   } catch (error) {
     error.statusCode = 400;
@@ -29,19 +32,36 @@ beans.get('/homeground', async (request, response, next) => {
   }
 });
 
-const fetchFullDetails = async (beansPathArray) => {
+const fetchFullDetails = async (beansPathArray, baseUrl) => {
   const beansInformation = [];
   for (let i = 0; i < beansPathArray.length; i++) {
-    const link: string = HOMEGROUND_BASE_URL + beansPathArray[i];
+    const link: string = baseUrl + beansPathArray[i];
     const content: axios.AxiosResponse = await axios.get(link);
     const $: cheerio.CheerioAPI = cheerio.load(content.data);
-    const name: string = $('.product-single__title').text().trim();
-    const details = createDetailsObject($);
-    beansInformation.push({
-      name: name,
-      link: link,
-      details,
-    });
+    let name: string;
+    let details;
+    switch (baseUrl) {
+      case HOMEGROUND_BASE_URL:
+        name = $('.product-single__title').text().trim();
+        details = createDetailsObject($);
+        beansInformation.push({
+          name: name,
+          link: link,
+          details,
+        });
+        break;
+      case ALCHEMIST_BASE_URL:
+        name = $('.ProductItem-details-title').text().trim();
+        details = createDetailsObject2($);
+        beansInformation.push({
+          name: name,
+          link: link,
+          details,
+        });
+        break;
+      default:
+        break;
+    }
   }
   return beansInformation;
 };
@@ -84,6 +104,56 @@ const createDetailsObject = ($) => {
   return details;
 };
 
+const createDetailsObject2 = ($) => {
+  const count = $('.ProductItem-details-excerpt > p').length - 3;
+  const details = $('.ProductItem-details-excerpt > p')
+    .eq(count)
+    .get()
+    .map((bean) => $(bean).text())
+    .reduce((detailsObject: {}, detailString) => {
+      if (detailString.indexOf('Origin') !== -1) {
+        detailsObject['process'] = detailString.substring(
+          detailString.indexOf('Process') + 'Process'.length + 2,
+          detailString.indexOf('Varietal')
+        );
+        detailsObject['varietal'] = detailString.substring(
+          detailString.indexOf('Varietal') + 'Varietal'.length + 2,
+          detailString.indexOf('Region')
+        );
+        detailsObject['region'] = detailString.substring(
+          detailString.indexOf('Region') + 'Region'.length + 2,
+          detailString.indexOf('Origin')
+        );
+        detailsObject['origin'] = detailString.substring(
+          detailString.indexOf('Origin') + 'Origin'.length + 2,
+          detailString.indexOf('Tasting Notes')
+        );
+        detailsObject['tastingNotes'] = detailString.substring(
+          detailString.indexOf('Tasting Notes') + 'Tasting Notes'.length + 2
+        );
+      } else {
+        detailsObject['process'] = detailString.substring(
+          detailString.indexOf('Process') + 'Process'.length + 2,
+          detailString.indexOf('Varietal')
+        );
+        detailsObject['varietal'] = detailString.substring(
+          detailString.indexOf('Varietal') + 'Varietal'.length + 2,
+          detailString.indexOf('Region')
+        );
+        detailsObject['region'] = detailString.substring(
+          detailString.indexOf('Region') + 'Region'.length + 2,
+          detailString.indexOf('Tasting Notes')
+        );
+        detailsObject['origin'] = 'N.A.';
+        detailsObject['tastingNotes'] = detailString.substring(
+          detailString.indexOf('Tasting Notes') + 'Tasting Notes'.length + 2
+        );
+      }
+      return detailsObject;
+    }, {});
+  return details;
+};
+
 const convertToCamelCase = (string) => {
   const returnArray: string[] = [];
   string.split(' ').forEach((word, index) => {
@@ -98,38 +168,19 @@ const convertToCamelCase = (string) => {
 
 beans.get('/alchemist', async (request, response, next) => {
   try {
-    const { data } = await axios.get(
+    const listOfBeans: axios.AxiosResponse = await axios.get(
       ALCHEMIST_BASE_URL + ALCHEMIST_FILTER_PATH
     );
-    const $ = cheerio.load(data);
-    const listItems = $('.hentry');
-    let results: string[][] = []!;
-    listItems.each((index, element) => {
-      let name: string = $(element)
-        .children('.grid-meta-wrapper')
-        .children('.grid-main-meta')
-        .children('.grid-title')
-        .text()
-        .trim();
-      let price: string = $(element)
-        .children('.grid-meta-wrapper')
-        .children('.grid-main-meta')
-        .children('.grid-prices')
-        .text()
-        .trim()
-        .replace(/from/g, '')
-        .trim();
-      let url: string = $(element).children('.grid-item-link').attr('href')!;
-      let status: string =
-        $(element)
-          .children('.grid-meta-wrapper')
-          .children('.grid-meta-status')
-          .children('.product-mark')
-          .text()
-          .trim() || 'Available';
-      updateResults(name, price, url, status, results);
-    });
-    response.status(200).json(results);
+    const $ = cheerio.load(listOfBeans.data);
+    const beansPathArray = $('.grid-item')
+      .not('.sold-out')
+      .get()
+      .map((bean) => $(bean).find('.grid-item-link').attr('href'));
+    const fullDetails = await fetchFullDetails(
+      beansPathArray,
+      ALCHEMIST_BASE_URL
+    );
+    response.status(200).json(fullDetails);
   } catch (error) {
     error.statusCode = 400;
     next(error);
